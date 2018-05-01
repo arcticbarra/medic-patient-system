9350653764043bec6575f2ecffd87b5203b315ea
Enrique Barragan <ebarragan1997@gmail.com>
2018-05-01 10:03:15 -0500

    change query

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -14,7 +14,7 @@ class Doctor < ApplicationRecord
   def select_new_leader
     last_area = Area.find(area_id_was)
     if areas.map(&:name).include? last_area.name
-      doctor = Doctor.where(speciality: last_area.name.downcase).where.not(id: id).first
+      doctor = Doctor.where("'#{last_area.name.downcase}' = ANY(speciality)").where.not(id: id).first
       last_area.update(doctor: doctor)
     end
   end

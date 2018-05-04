64b527156c5612dcfc76c7a56f23cb31916caa2e
Enrique Barragan <ebarragan1997@gmail.com>
2018-05-04 16:16:31 -0500

    add check on speciality

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -5,17 +5,24 @@ class Doctor < ApplicationRecord
   before_update :increase_salary, if: :years_experience_changed?
   before_update :select_new_leader, if: :area_id_changed?
   has_many :treatments
-  # enum speciality: %i[general traumatology allergology
-  #                     radiology cardiology gerontology
-  #                     obstetrics pediatrics
-  #                   ]
   validate :speciality_in_area
+  validate :valid_specialities
 
   def select_new_leader
     last_area = Area.find(area_id_was)
     if areas.map(&:name).include? last_area.name
       doctor = Doctor.where(area: last_area).where.not(id: id).first
       last_area.update(doctor: doctor)
+    end
+  end
+
+  def valid_specialities
+    valid_specs = %w[general traumatology allergology
+                      radiology cardiology gerontology
+                      obstetrics pediatrics
+                    ]
+    speciality.each do |spec|
+      errors.add(:invalid_speciality, "#{spec} is not a valid speciality") unless valid_specs.include?(spec)
     end
   end
 
